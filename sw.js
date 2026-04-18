e3a448c0d47fee61966bfd1d14ac4eb6025dec81
telekarinho <40647488+telekarinho@users.noreply.github.com>
2026-04-18 14:59:39 -0300

    Cache-buster v11
    Co-authored-by: telekarinho <telekarinho@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'mp-v10';
-const CACHE_NAME = 'milkypot-v10';
+const CACHE_VERSION = 'mp-v11';
+const CACHE_NAME = 'milkypot-v11';
 
 // Critical local assets that must be available offline
 const PRECACHE_URLS = [
